136e1da318220a7a3862bcb09182965314db4e09
ahesh001 <ahesh001@plattsburgh.edu>
2025-09-01 11:00:15 -0400

    Testing Scroll to Top function. Home page came back once I removed the useEffect variable. Testing to see if this is the main issue

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import SmartBoxPage from './components/SmartBoxPage';
 // Scroll to top on route change
 function ScrollToTop() {
   const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
   return null;
 }
 
